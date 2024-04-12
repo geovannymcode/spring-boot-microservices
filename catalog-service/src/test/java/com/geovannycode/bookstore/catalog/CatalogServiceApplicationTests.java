fdb625a49dd493233d56d24fa3ee3589dec9332c
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-04-11 22:11:49 -0500

    fix(catalogservicesapplicationtest) setting test

--- catalog-service/src/test/java/com/geovannycode/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/geovannycode/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.geovannycode.bookstore.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(ContainersConfig.class)
 class CatalogServiceApplicationTests {
 
     @Test
