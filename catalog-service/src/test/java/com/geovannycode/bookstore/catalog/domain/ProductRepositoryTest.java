b12aaa418316b951fc240465b15181a2e991078b
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-04-14 11:58:33 -0500

    feat(all): Implement Get Products API with pagination

---
+++ catalog-service/src/test/java/com/geovannycode/bookstore/catalog/domain/ProductRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.geovannycode.bookstore.catalog.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJpaTest(
+        properties = {
+            "spring.test.database.replace=none",
+            "spring.datasource.url=jdbc:tc:postgresql:16-alpine:///db",
+        })
+@Sql("/test-data.sql")
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void shouldGetAllProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+        assertThat(products).hasSize(15);
+    }
+}
