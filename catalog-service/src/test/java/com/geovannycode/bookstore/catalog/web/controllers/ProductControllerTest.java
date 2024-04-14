11808a40ff9f14b18c4e80f58eefb79fcc82657a
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-04-14 01:03:26 -0500

    feat(all) add test

---
+++ catalog-service/src/test/java/com/geovannycode/bookstore/catalog/web/controllers/ProductControllerTest.java
@@ -0,0 +1,31 @@
+package com.geovannycode.bookstore.catalog.web.controllers;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import com.geovannycode.bookstore.catalog.AbstractIT;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
+
+@Sql("/test-data.sql")
+public class ProductControllerTest extends AbstractIT {
+
+    @Test
+    void shouldReturnProducts() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/products")
+                .then()
+                .statusCode(200)
+                .body("data", hasSize(10))
+                .body("totalElements", is(15))
+                .body("pageNumber", is(1))
+                .body("totalPages", is(2))
+                .body("isFirst", is(true))
+                .body("isLast", is(false))
+                .body("hasNext", is(true))
+                .body("hasPrevious", is(false));
+    }
+}
