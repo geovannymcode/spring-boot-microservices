3d499d4b5b03dccc069a4f326489b52da51811cc
Geovanny Mendoza <Geovanny0401@users.noreply.github.com>
2024-04-20 15:02:22 -0500

    feat(all): Implement New Orders Processing Job

--- order-service/src/main/java/com/geovannycode/bookstore/orders/domain/OrderRepository.java
+++ order-service/src/main/java/com/geovannycode/bookstore/orders/domain/OrderRepository.java
@@ -1,5 +1,18 @@
 package com.geovannycode.bookstore.orders.domain;
 
+import com.geovannycode.bookstore.orders.domain.models.OrderStatus;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {}
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+    List<OrderEntity> findByStatus(OrderStatus status);
+
+    Optional<OrderEntity> findByOrderNumber(String orderNumber);
+
+    default void updateOrderStatus(String orderNumber, OrderStatus status) {
+        OrderEntity order = this.findByOrderNumber(orderNumber).orElseThrow();
+        order.setStatus(status);
+        this.save(order);
+    }
+}
--- order-service/src/main/java/com/geovannycode/bookstore/orders/domain/OrderService.java
+++ order-service/src/main/java/com/geovannycode/bookstore/orders/domain/OrderService.java
@@ -3,6 +3,7 @@ package com.geovannycode.bookstore.orders.domain;
 import com.geovannycode.bookstore.orders.domain.models.CreateOrderRequest;
 import com.geovannycode.bookstore.orders.domain.models.CreateOrderResponse;
 import com.geovannycode.bookstore.orders.domain.models.OrderCreatedEvent;
+import com.geovannycode.bookstore.orders.domain.models.OrderStatus;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,5 +36,38 @@ public class OrderService {
         OrderCreatedEvent orderCreatedEvent = OrderEventMapper.buildOrderCreatedEvent(savedOrder);
         orderEventService.save(orderCreatedEvent);
         return new CreateOrderResponse(savedOrder.getOrderNumber());
+    }
+
+    public void processNewOrders() {
+        List<OrderEntity> orders = orderRepository.findByStatus(OrderStatus.NEW);
+        log.info("Found {} new orders to process", orders.size());
+        for (OrderEntity order : orders) {
+            this.process(order);
+        }
+    }
+
+    private void process(OrderEntity order) {
+        try {
+            if (canBeDelivered(order)) {
+                log.info("OrderNumber: {} can be delivered", order.getOrderNumber());
+                orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.DELIVERED);
+                orderEventService.save(OrderEventMapper.buildOrderDeliveredEvent(order));
+
+            } else {
+                log.info("OrderNumber: {} can not be delivered", order.getOrderNumber());
+                orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.CANCELLED);
+                orderEventService.save(
+                        OrderEventMapper.buildOrderCancelledEvent(order, "Can't deliver to the location"));
+            }
+        } catch (RuntimeException e) {
+            log.error("Failed to process Order with orderNumber: {}", order.getOrderNumber(), e);
+            orderRepository.updateOrderStatus(order.getOrderNumber(), OrderStatus.ERROR);
+            orderEventService.save(OrderEventMapper.buildOrderErrorEvent(order, e.getMessage()));
+        }
+    }
+
+    private boolean canBeDelivered(OrderEntity order) {
+        return DELIVERY_ALLOWED_COUNTRIES.contains(
+                order.getDeliveryAddress().country().toUpperCase());
     }
 }
